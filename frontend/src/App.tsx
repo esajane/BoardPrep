579c55fba5f0955715af201149da8b5240665046
aexizn <janessa.pandiyan@cit.edu>
2023-12-01 14:45:38 +0800

    Esa pushing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import Courselist from "./pages/Courselist";
 import Searchbar from "./components/Searchbar";
-import "./styles/testing.css";
+import "./styles/testing.scss";
 import Carousel from "./components/Carousel";
 import Syllabus from "./components/Syllabus";
 import CourseMain from "./components/CourseMain";
 import CourseDetails from "./pages/CourseDetails";
 import CourseList from "./pages/Courselist";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LessonPage from "./pages/LessonPage";
 
 function App() {
   return (
