52510678457163510f672315cf00305b34e22cae
aexizn <janessa.pandiyan@cit.edu>
2023-12-11 03:57:57 +0800

    Updated App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,9 @@ import Courselist from "./pages/Courselist";
 import "./styles/testing.scss";
 import Syllabus from "./components/Syllabus";
 import MockTest from "./pages/Mocktest";
+import CourseDetails from "./pages/CourseDetails";
+
+import AdminDashboard from "./pages/AdminCourse";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Classes from "./pages/Classes";
 import Classroom from "./pages/Classroom";
@@ -16,6 +19,9 @@ import PrivateRoute from "./components/PrivateRoute";
 import PublicRoute from "./components/PublicRoute";
 import { setUserFromLocalStorage } from "./redux/slices/authSlice";
 import { useAppDispatch } from "./redux/hooks";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+//import MockTestManage from "./pages/MockTestManage";
 
 function App() {
   const dispatch = useAppDispatch();
@@ -90,6 +96,37 @@ function App() {
             </PublicRoute>
           }
         />
+        <Route
+          path="/content"
+          element={
+            <PrivateRoute>
+              <Provider store={store}>
+                <AdminDashboard />
+              </Provider>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/courses/:courseId/edit"
+          element={
+            <PrivateRoute>
+              <Provider store={store}>
+                <CourseDetails />
+              </Provider>
+            </PrivateRoute>
+          }
+        />
+        {/* <Route
+          path="/mocktest/:mocktestID/manage"
+          element={
+            <PublicRoute>
+              <Provider store={store}>
+                <MockTestManage />
+              </Provider>
+            </PublicRoute>
+          }
+        />
+      </Routes> */}
       </Routes>
     </Router>
   );
